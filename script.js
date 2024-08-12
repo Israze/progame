76b828834b9f1f1deda5796fa9fac526c575e7aa
Israel <obafemiadeduro@gmail.com>
2024-08-12 04:03:32 +0100

    testing again

--- script.js
+++ script.js
@@ -44,5 +44,3 @@ document.addEventListener('click', function(event) {
 document.getElementById('project-button').addEventListener('click', function() {
     window.location.href = 'projects.html';
   });
-
-  console.log("bitch")
